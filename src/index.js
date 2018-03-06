7493ffefea5536185c71ac8614d1f0b529e0b0e0
Michael Anderson <michanders@msn.com>
2018-03-06 20:59:45 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import { createStore } from 'redux'
 import counterReducer from './reducers/counterReducer'
 
 const store = createStore(counterReducer)
+<<<<<<< HEAD
+=======
+
+
+console.log(store.getState())
+store.dispatch({type: 'INCREMENT_COUNT'})
+store.dispatch({type: 'INCREMENT_COUNT'})
+console.log(store.getState())
+store.dispatch({type: 'DECREMENT_COUNT'})
+console.log(store.getState())
+store.dispatch({type: 'INCREMENT_COUNT'})
+store.dispatch({type: 'INCREMENT_COUNT'})
+console.log(store.getState())
+store.dispatch({type: 'RESET_COUNT'})
+console.log(store.getState())
+>>>>>>> 4d4dd4601ad2b1efbd7aff8bfe6675326333ae8d
